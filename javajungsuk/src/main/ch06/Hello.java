e0e55d74b220f0214cff7056e18a68d92cac9088
dasom2646 <choi.dasom2646@gmail.com>
2023-02-15 15:07:01 +0900

    ch6 객체지향

---
+++ javajungsuk/src/main/ch06/Hello.java
@@ -0,0 +1,6 @@
+package main.ch06;
+
+// 1. 하나의 소스파일에는 하나의 클래스만 작성하는것이 바람직.
+public class Hello {}  // 소스파일의 이름은 public class이름과 일치해야한다.
+class Hello2 {} // 하나의 소스파일에는 하나늬 public class만 허용한다.
+class Hello3 {}
