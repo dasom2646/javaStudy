091c4e7ac576d1ede8b6deeac428bc82c66382a3
dasom2646 <choi.dasom2646@gmail.com>
2023-02-20 15:12:34 +0900

    ch8 예외처리

---
+++ javajungsuk/src/main/ch08/Ex8_09.java
@@ -0,0 +1,16 @@
+package main.ch08;
+
+public class Ex8_09 {
+
+	public static void main(String[] args) throws Exception {
+		method1(); // 같은 클래스내의 static멤버이므로 객체생성없이 직접 호출가능.
+	}
+
+	static void method1() throws Exception {
+		method2();
+	}
+
+	static void method2() throws Exception {
+		throw new Exception();
+	}
+}
