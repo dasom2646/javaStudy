af145a772a27843a03a81970318787c06d30fd8a
dasom2646 <choi.dasom2646@gmail.com>
2023-02-21 17:56:16 +0900

    ch9

---
+++ javajungsuk/src/main/ch09/Ex9_08.java
@@ -0,0 +1,15 @@
+package main.ch09;
+
+public class Ex9_08 {
+
+	public static void main(String[] args) {
+		// 길이가 0인 char배열을 생성한다.
+		char[] cArr = new char[0]; // char[] cArr = {};와 같다.
+		String s = new String(cArr); // String s = new String("");와 같다.
+
+		System.out.println("cArr.lenght=" + cArr.length);
+		System.out.println("@@@" + s + "@@@");
+
+	}
+
+}
