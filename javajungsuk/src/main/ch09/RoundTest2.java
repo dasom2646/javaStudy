7864228885da8042fc240746b3f0261e669557dd
dasom2646 <choi.dasom2646@gmail.com>
2023-02-21 18:43:45 +0900

    ch9 java.lang패키지와 유용한 클래스

---
+++ javajungsuk/src/main/ch09/RoundTest2.java
@@ -0,0 +1,11 @@
+package main.ch09;
+
+public class RoundTest2 {
+	public static void main(String[] args) {
+//		int i =Integer.parseInt("100");
+		System.out.println("i=" +Integer.parseInt("100"));
+		System.out.println("i=" +Integer.parseInt("100",10)); // 위와 동일
+		System.out.println("i=" +Integer.parseInt("100",2)); // 2진수
+		System.out.println("i=" +Integer.parseInt("FF",2)); // 16진수
+	}
+}
